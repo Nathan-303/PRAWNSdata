5e8bc5bb4a70f186f82b7fcf2f4776336c0822c4
Nathan Gray <nrg508@york.ac.uk>
2023-06-06 15:14:00 +0100

    cut a lot of scores, ranks and redundant codes to deduce size

--- data-raw/prep_data.R
+++ data-raw/prep_data.R
@@ -25,24 +25,40 @@ LSOA_demographics <- read.csv(here::here("data-raw","LSOA_statistics","2019_LSOA
   inner_join(city_data,by="LSOA11CD")%>%
   inner_join(rural_urban,by=c("LSOA11CD"="LSOA11CD"))
 
-#Links the demographic data to the references to the shapefile
-demo_linked_reference <- inner_join(city_data,LSOA_demographics,by=c("LSOA11CD"="LSOA11CD","FID"="FID","TCITY15CD"="TCITY15CD",
-                                                                     "TCITY15NM"="TCITY15NM"))
-
 #Reads the county lookup data
 county_lookup <- read.csv(here::here("data-raw","LSOA_statistics","county lookup 2019.csv"),row.names = 1)
 
 #Makes a chunk for the LSOAs that are in counties, then mutates in a column saying it's a couty not UA
-county_chunk <- inner_join(county_lookup,demo_linked_reference,by=c("LAD19NM"="Local.Authority.District.name..2019.")) %>%
-  mutate(Area_Type="County") %>%
-  rename("Area"=CTY19NM)
+county_list <- (LSOA_demographics$Local.Authority.District.name..2019. %in% county_lookup$LAD19NM)
+
+county_chunk <- LSOA_demographics[county_list,] %>%
+  mutate(Area_Type="Unitary Authority",Area=Local.Authority.District.name..2019.)
+
 
 unitary_list <- !(LSOA_demographics$Local.Authority.District.name..2019. %in% county_lookup$LAD19NM)
 
 unitary_chunk <- LSOA_demographics[unitary_list,] %>%
-  mutate(Area_Type="Unitary Authority",Area=Local.Authority.District.name..2019.)# %>%
-#inner_join(demo_linked_reference,by="LSOA11CD")
-refined_chunk <- bind_rows(unitary_chunk,county_chunk) %>% tibble()
+  mutate(Area_Type="Unitary Authority",Area=Local.Authority.District.name..2019.)
+
+refined_chunk <- bind_rows(unitary_chunk,county_chunk) %>% tibble() %>%
+  dplyr::select(-c(Income.Score..rate.,Income.Rank..where.1.is.most.deprived.,Employment.Score..rate.,
+                   Employment.Rank..where.1.is.most.deprived.,Education..Skills.and.Training.Score,
+                   Education..Skills.and.Training.Rank..where.1.is.most.deprived.,Health.Deprivation.and.Disability.Score,
+                   Health.Deprivation.and.Disability.Rank..where.1.is.most.deprived.,Crime.Score,
+                   Crime.Rank..where.1.is.most.deprived.,Barriers.to.Housing.and.Services.Score,Barriers.to.Housing.and.Services.Rank..where.1.is.most.deprived.,
+                   Living.Environment.Score,Living.Environment.Rank..where.1.is.most.deprived.,Income.Deprivation.Affecting.Children.Index..IDACI..Score..rate.,
+                   Income.Deprivation.Affecting.Children.Index..IDACI..Rank..where.1.is.most.deprived.,Income.Deprivation.Affecting.Older.People..IDAOPI..Score..rate.,
+                   Income.Deprivation.Affecting.Older.People..IDAOPI..Rank..where.1.is.most.deprived.,
+                   Children.and.Young.People.Sub.domain.Score,Children.and.Young.People.Sub.domain.Rank..where.1.is.most.deprived.,
+                   Adult.Skills.Sub.domain.Score,Adult.Skills.Sub.domain.Rank..where.1.is.most.deprived.,
+                   Geographical.Barriers.Sub.domain.Score,Geographical.Barriers.Sub.domain.Rank..where.1.is.most.deprived.,
+                   Wider.Barriers.Sub.domain.Score,Wider.Barriers.Sub.domain.Rank..where.1.is.most.deprived.,
+                   Indoors.Sub.domain.Score,Indoors.Sub.domain.Rank..where.1.is.most.deprived.,Outdoors.Sub.domain.Score,
+                   Outdoors.Sub.domain.Rank..where.1.is.most.deprived.,LSOA11NM.x,
+                   LSOA11NM.y,FID,Index.of.Multiple.Deprivation..IMD..Score,LSOA.name..2011.,
+                   Local.Authority.District.code..2019.,Index.of.Multiple.Deprivation..IMD..Rank..where.1.is.most.deprived.,
+                   TCITY15CD
+  ))
 rm(unitary_list)
 rm(unitary_chunk)
 rm(county_chunk)
