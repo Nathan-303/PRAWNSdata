e6762c340bd1ff31ea75351dfcd8c3774d5c19bb
Nathan Gray <nrg508@york.ac.uk>
2023-06-06 14:29:49 +0100

    Revert "wave1"
    This reverts commit a6fce30cdf0fdb5c3502b2fd5c2225e36c10763a.

--- data-raw/prep_data.R
+++ data-raw/prep_data.R
@@ -4,7 +4,6 @@ library(devtools)
 library(usethis)
 library(roxygen2)
 library(here)
-library(sf)
 #Write the shapefile to data
 LSOA_shapefile <- read_sf(here::here("data-raw","2011_LSOA_shapefile_20m_generalised"))
 usethis::use_data(LSOA_shapefile, overwrite = TRUE,compress = "xz")
@@ -41,17 +40,9 @@ county_chunk <- inner_join(county_lookup,demo_linked_reference,by=c("LAD19NM"="L
 unitary_list <- !(LSOA_demographics$Local.Authority.District.name..2019. %in% county_lookup$LAD19NM)
 
 unitary_chunk <- LSOA_demographics[unitary_list,] %>%
-  mutate(Area_Type="Unitary Authority",Area=Local.Authority.District.name..2019.) %>%
- inner_join(demo_linked_reference,by="LSOA11CD")
-refined_chunk <- bind_rows(unitary_chunk,county_chunk) %>% tibble() %>%
-  dplyr::select(-c(Income.Score..rate.,Income.Rank..where.1.is.most.deprived.,Employment.Score..rate.,
-                   Employment.Rank..where.1.is.most.deprived.,Education..Skills.and.Training.Score,
-                   Education..Skills.and.Training.Rank..where.1.is.most.deprived.,Health.Deprivation.and.Disability.Score,
-                   Health.Deprivation.and.Disability.Rank..where.1.is.most.deprived.,Crime.Score,
-                   Crime.Rank..where.1.is.most.deprived.,Barriers.to.Housing.and.Services.Score,Barriers.to.Housing.and.Services.Rank..where.1.is.most.deprived.,
-                   Living.Environment.Score,Living.Environment.Rank..where.1.is.most.deprived.,Income.Deprivation.Affecting.Children.Index..IDACI..Score..rate.,
-                   Income.Deprivation.Affecting.Children.Index..IDACI..Rank..where.1.is.most.deprived.,Income.Deprivation.Affecting.Older.People..IDAOPI..Score..rate.,Income.Deprivation.Affecting.Older.People..IDAOPI..Rank..where.1.is.most.deprived.,Children.and.Young.People.Sub.domain.Score,Children.and.Young.People.Sub.domain.Rank..where.1.is.most.deprived.,Adult.Skills.Sub.domain.Score,Adult.Skills.Sub.domain.Rank..where.1.is.most.deprived.,Geographical.Barriers.Sub.domain.Score,Geographical.Barriers.Sub.domain.Rank..where.1.is.most.deprived.,Wider.Barriers.Sub.domain.Score,Wider.Barriers.Sub.domain.Rank..where.1.is.most.deprived.,Indoors.Sub.domain.Score,Indoors.Sub.domain.Rank..where.1.is.most.deprived.,Outdoors.Sub.domain.Score,Outdoors.Sub.domain.Rank..where.1.is.most.deprived.,LSOA11NM.x,LSOA11NM.y
-  ))
+  mutate(Area_Type="Unitary Authority",Area=Local.Authority.District.name..2019.)# %>%
+#inner_join(demo_linked_reference,by="LSOA11CD")
+refined_chunk <- bind_rows(unitary_chunk,county_chunk) %>% tibble()
 rm(unitary_list)
 rm(unitary_chunk)
 rm(county_chunk)
