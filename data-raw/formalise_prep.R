094a33a2d523f6bd24e2d1944f44085fbc27ecc9
Nathan Gray <nrg508@york.ac.uk>
2024-04-17 11:30:42 +0100

    listsets added

--- data-raw/formalise_prep.R
+++ data-raw/formalise_prep.R
@@ -1,5 +1,9 @@
 usethis::use_r("LSOA_shapefile")
 usethis::use_r("refined_chunk")
 usethis::use_r("pollutant_key")
+usethis::use_r("ListSets")
 devtools::document()
 devtools::install()
+
+library(devtools)
+install_github("Nathan-303/PRAWNSdata",upgrade = "never")
